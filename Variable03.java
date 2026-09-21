576decd68b27db2c4b10cf40f153fb01a1004a0f
graciawasispuspitaharum <graciacia204@gmail.com>
2026-09-21 19:32:38 +0700

    Add files via upload

---
+++ Variable03.java
@@ -0,0 +1,25 @@
+public class Variable03 {
+    public static void main(String[] args) {
+        // Deklarasi Variable
+        String nama1, nama2, nama3, nama4;
+        int NIM1, NIM2, NIM3, NIM4;
+        
+        // Isi Nilai Variable
+        nama1 = "Gracia Wasis P.H.";
+        nama2 = "Naila";
+        nama3 = "Griselda";
+        nama4 = "Siti Tasya";
+        NIM1 = 123456;
+        NIM2 = 456789;
+        NIM3 = 678901;
+        NIM4 = 890123;
+        System.out.println("================================");
+        System.out.println("NIM    |    Nama");
+        System.out.println("================================");
+        System.out.println(NIM1 + " | " + nama1);
+        System.out.println(NIM2 + " | " + nama2);
+        System.out.println(NIM3 + " | " + nama3);
+        System.out.println(NIM4 + " | " + nama4);
+        System.out.println("================================");
+    }
+}
---
+++ Varible4.java
@@ -0,0 +1,15 @@
+public class Varible4 {
+    public static void main(String[] args) {
+        int bilangan1, bilangan2;
+        
+        bilangan1 = 10;
+        bilangan2 = 5;
+
+        System.out.println("Bilangan 1 = " + bilangan1);
+        System.out.println("Bilangan 2 = " + bilangan2);
+        System.out.println("Hasil Tambah : " + (bilangan1 + bilangan2));
+        System.out.println("Hasil Kurang : " + (bilangan1 - bilangan2));
+        System.out.println("Hasil Kali : " + (bilangan1 * bilangan2));
+        System.out.println("Hasil Bagi : " + (bilangan1 / bilangan2));
+    }
+}
